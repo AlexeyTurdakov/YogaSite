12770fb227dfcd6c4d301bff57afc989e145af07
Alexey <alexeyturdakov@gmail.com>
2021-08-19 12:24:47 +0300

    Add Calculator

--- js/script.js
+++ js/script.js
@@ -112,7 +112,8 @@ window.addEventListener('DOMContentLoaded', function () {
     knowMore.forEach(item => item.addEventListener('click', callModalWindow));
     close.addEventListener('click', callModalWindow);
 
-    //form
+    //Form
+
     let message = {
         loading: 'Загрузка...',
         success: 'Ваша заявка принята! Спасибо! Скоро мы с вами свяжемся!',
@@ -178,7 +179,7 @@ window.addEventListener('DOMContentLoaded', function () {
         next = document.querySelector('.next'),
         dotsWrap = document.querySelector('.slider-dots'),
         dots = document.querySelectorAll('.dot');
-    
+
     function showSlides(n) {
 
         if (n > slides.length) {
@@ -223,4 +224,41 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
     showSlides(slideIndex);
+
+    //Calc
+
+    let person = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        placeSum = 0;
+        
+    person.addEventListener('input', function () {
+        personsSum = +this.value;
+        calc(+this.value);
+    });
+
+    restDays.addEventListener('input', function () {
+        daysSum = +this.value;
+        calc();
+    });
+
+    place.addEventListener('input', function () {
+        placeSum = this.options[this.selectedIndex].value;
+        calc();
+    });
+
+    function calc() {
+        if (restDays.value == '' || person.value == '') {
+            totalValue.innerHTML = 'Вы не заполнили одно из полей!';
+        } else if (placeSum < 1) {
+            totalValue.innerHTML = 'Выберите направление!';
+        } else {
+            totalValue.innerHTML = personsSum * daysSum * 10000 * placeSum;
+        }
+    }
+
+
 });
